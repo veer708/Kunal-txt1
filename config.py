bcc3bdaccfc7c03f888db72212fe769173fde89d
sonic kuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-02-05 18:50:55 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+BOT_NAME = "Moni Uploader 2"
+API_ID    = 24495656
+API_HASH  = "61afcf68c6429714dd18acd07f246571"
+BOT_TOKEN = "8040014845:AAHAo3zgpikp-4j2PLQofMxSwtoOdQKTQLg"
+MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
